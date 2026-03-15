25a405e36a185cdc168fc71a333c70d8ff4b9321
monguis305 <taktashev2402@gmail.com>
2026-03-15 23:34:45 +1100

    Delete .github/workflows directory

--- .github/workflows/create_facts.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-df = pd.DataFrame({'fact': ['Нефть образовалась из древних организмов', 'Первая нефтяная скважина - 1859 год', 'Россия - лидер по добыче']})
-df.to_excel('facts.xlsx', index=False)
-print("facts.xlsx created")
